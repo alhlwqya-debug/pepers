b475747792d5fd6b5222a1f7fdfa1786a05e4741
alhlwqya-debug <alhlwqya@gmail.com>
2026-09-13 19:40:20 +0300

    اضافه المكتبات

--- app/src/main/kotlin/com/add/pepers/MainActivity.kt
+++ app/src/main/kotlin/com/add/pepers/MainActivity.kt
@@ -43,7 +43,6 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.lifecycleScope
 import com.add.pepers.cloud.AuthResult
 import com.add.pepers.cloud.AuthSession
 import com.add.pepers.cloud.SupabaseAuthRepository
