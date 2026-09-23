1c8d2f5e3de346772f4420f46259a6ac6b27ac77
alhlwqya-debug <alhlwqya@gmail.com>
2026-09-23 19:46:12 +0300

    fix: import Add and Edit Compose icons

--- app/src/main/kotlin/com/add/pepers/DrawerContent.kt
+++ app/src/main/kotlin/com/add/pepers/DrawerContent.kt
@@ -27,7 +27,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AlertDialog
